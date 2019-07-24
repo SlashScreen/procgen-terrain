1fcc51120f0621d74b65dec0e62c024ffd9d201c
GJ <TALLESTTOWER1@GMAIL.COM>
2019-07-24 12:41:54 -0700

    Completed transfer to class

--- mapgenutils.py
+++ mapgenutils.py
@@ -1,13 +1,71 @@
 #mapgenutils.py
 
 import math
+import perlin
+from PIL import Image
 
 class genmap:
-    def __init__(self, termap,terimg,d):
-        self.d = d
+    def __init__(self,dimensions, heightmod, heightboost, genBeach = True, printh = False, waterheight = 0):
+        ###GENERATE###
+        pn = perlin.PerlinNoiseFactory(2,octaves = 2)
+        termap = {}
+        terimg = Image.new("RGB",(dimensions,dimensions))
+        terpx = terimg.load()
+        for x in range(dimensions):
+            termap[x] = {}
+            for y in range(dimensions):
+                value = pn(x/(dimensions/2),y/(dimensions/2))*heightmod+heightboost
+                #print(value)
+                i = ""
+                p = (255,255,255)
+                if value <= waterheight:
+                    i = "r"
+                    
+                else:
+                    i = "g"
+                    p = (0,0,0)
+                termap[x][y] = i
+                terpx[x,y] = p
+
+        #pseudocode
+        #only blank and not blank
+        #then replace edges with #
+
+    #Idea: if more water than land, flip the water and land
+
+        if genBeach:
+            for x in range(dimensions-1):
+                for y in range(dimensions-1):
+                    if x > 0 and y > 0:
+                        if termap[x][y] == "g" and termap[x+1][y] == "r":
+                            termap[x][y] = "#"
+                            terpx[x,y] = (135,135,135)
+                        elif termap[x][y] == "g" and termap[x-1][y] == "r":
+                            termap[x][y] = "#"
+                            terpx[x,y] = (135,135,135)
+                        elif termap[x][y] == "g" and termap[x][y+1] == "r":
+                            termap[x][y] = "#"
+                            terpx[x,y] = (135,135,135)
+                        elif termap[x][y] == "g" and termap[x][y-1] == "r":
+                            termap[x][y] = "#"
+                            terpx[x,y] = (135,135,135)
+                            #New apprach: scan around pixel, if next over is not equal to this ine, set to beach
+
+        ###PRINT###
+        if printh:
+            for key,value in termap.items():
+                out = ""
+                for k,v in value.items():
+                    out = out+str(v)
+                print(out)
+
+            terimg.show()
+
         self.mp = termap
         self.img = terimg
+        self.d = dimensions
         
+            
     def render(self):
         #creates a new image for itself based on mp
 
--- procgen.py
+++ procgen.py
@@ -1,69 +1,8 @@
-import noise
-from PIL import Image
 import mapgenutils as gu
-import perlin
 
-def gen_baseterrain(dimensions, heightmod, heightboost, genBeach = True, printh = False, waterheight = 0):
-
-    ###GENERATE###
-    pn = perlin.PerlinNoiseFactory(2,octaves = 6)
-    termap = {}
-    terimg = Image.new("RGB",(dimensions,dimensions))
-    terpx = terimg.load()
-    for x in range(dimensions):
-        termap[x] = {}
-        for y in range(dimensions):
-            value = pn(x/dimensions,y/dimensions)*heightmod+heightboost
-            #print(value)
-            i = ""
-            p = (255,255,255)
-            if value <= waterheight:
-                i = "r"
-                
-            else:
-                i = "g"
-                p = (0,0,0)
-            termap[x][y] = i
-            terpx[x,y] = p
-
-    #pseudocode
-    #only blank and not blank
-    #then replace edges with #
-
-    if genBeach:
-        for x in range(dimensions-1):
-            for y in range(dimensions-1):
-                if x > 0 and y > 0:
-                    if termap[x][y] == "g" and termap[x+1][y] == "r":
-                        termap[x][y] = "#"
-                        terpx[x,y] = (135,135,135)
-                    elif termap[x][y] == "g" and termap[x-1][y] == "r":
-                        termap[x][y] = "#"
-                        terpx[x,y] = (135,135,135)
-                    elif termap[x][y] == "g" and termap[x][y+1] == "r":
-                        termap[x][y] = "#"
-                        terpx[x,y] = (135,135,135)
-                    elif termap[x][y] == "g" and termap[x][y-1] == "r":
-                        termap[x][y] = "#"
-                        terpx[x,y] = (135,135,135)
-                        #New apprach: scan around pixel, if next over is not equal to this ine, set to beach
-
-    ###PRINT###
-    if printh:
-        for key,value in termap.items():
-            out = ""
-            for k,v in value.items():
-                out = out+str(v)
-            print(out)
-
-        terimg.show()
-
-    gmp = gu.genmap(termap,terimg,dimensions)
-    
-    return gmp
 
 if __name__ == "__main__" :
-    test = gen_baseterrain(150,.5,1,waterheight = 1)
+    test = gu.genmap(350,4,1,waterheight = .2)
     test.drawCircle(10,10,6,"r")
     test.drawRect(15,15,25,25,"r")
     test.render()
