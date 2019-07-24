03b50aebc2ec0572b552011e6f6acee67086254e
GJ <TALLESTTOWER1@GMAIL.COM>
2019-07-24 08:32:19 -0700

    It actually draws the circle well. Now, how to fill it?...

--- mapgenutils.py
+++ mapgenutils.py
@@ -11,22 +11,16 @@ class genmap:
         
     def render(self):
         #creates a new image for itself based on mp
- #       temp = self.img
- #       temppx = temp.load()
+
         for x in range(self.d):
             for y in range(self.d):
                 if self.mp[x][y] == "r":
-                    print("r")
                     self.img.putpixel((x,y),(255,255,255))
                 elif self.mp[x][y] == "#":
-                    print("#")
                     self.img.putpixel((x,y),(135,135,135))
                 else:
-                    print("g")
                     self.img.putpixel((x,y),(0,0,0))
- #       print(temppx)
- #       self.img = temp
- #       temp.show()
+
         self.img.show()
                     
     def crop(self,d):
--- procgen.py
+++ procgen.py
@@ -62,7 +62,7 @@ def gen_baseterrain(dimensions, heightmod, heightboost, genBeach = True, printh 
 
 if __name__ == "__main__" :
     test = gen_baseterrain(50,4,2)
-    test.drawCircle(10,10,6,"#")
+    test.drawCircle(10,10,6,"r")
     test.drawRect(15,15,25,25,"r")
     test.render()
     test.showimg()
